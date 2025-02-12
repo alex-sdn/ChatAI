7b2790f15f1f2a1db167f58d57e0883ae047f29f
Alexandre S <alexsdnwcz@gmail.com>
2025-02-12 19:52:44 +0100

    routes with react-router-dom

---
+++ client/src/layouts/rootLayout/RootLayout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const RootLayout = () => {
+  return (
+    <div>
+      <div>
+        RootLayout
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default RootLayout
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,45 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from './layouts/rootLayout/RootLayout.tsx'
+import DashboardPage from './pages/dashboardPage/DashboardPage.tsx'
+import ChatPage from './pages/chatPage/ChatPage.tsx'
+import AuthLayout from './layouts/authLayout/AuthLayout.tsx'
+import SignInPage from './pages/signInPage/SignInPage.tsx'
+import SignUpPage from './pages/signUpPage/SignUpPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <DashboardPage />
+      },
+      {
+        path: '/chat/:id',
+        element: <ChatPage />
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignInPage />
+      },
+      {
+        path: '/sign-up',
+        element: <SignUpPage />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
