ea013e928bbc8568fe5205ef9c0e7f9a489fce88
Alexandre S <alexsdnwcz@gmail.com>
2025-03-12 18:30:52 +0100

    move prompt to sep component

---
+++ client/src/components/NewPrompt.tsx
@@ -0,0 +1,19 @@
+
+export default function NewPrompt() {
+  return (
+    <div className="max-w-full w-[740px]">
+      <form className="mt-[20px] mx-[16px] pb-[30px] pt-[8px] px-[16px] bg-[#333333] rounded-[14px]">
+        <textarea
+          name="prompt"
+          placeholder="Ask anything..."
+          className="w-full resize-none overflow-hidden focus:outline-none"
+          rows={1}
+          onInput={(e) => {
+            e.currentTarget.style.height = 'auto';
+            e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
+          }}
+        />
+      </form>
+    </div>
+  )
+}
--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,3 +1,4 @@
+import NewPrompt from "../components/NewPrompt"
 
 const DashboardPage = () => {
   return (
@@ -5,20 +6,7 @@ const DashboardPage = () => {
       <h1 className="font-semibold text-3xl text-center">
         What can I help with?
       </h1>
-      <div className="max-w-full w-[740px]">
-        <form className="mt-[20px] mx-[16px] pb-[30px] pt-[8px] px-[16px] bg-[#333333] rounded-[14px]">
-          <textarea
-            name="prompt"
-            placeholder="Ask anything..."
-            className="w-full resize-none overflow-hidden focus:outline-none"
-            rows={1}
-            onInput={(e) => {
-              e.currentTarget.style.height = 'auto';
-              e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
-            }}
-          />
-        </form>
-      </div>
+      <NewPrompt />
     </div>
   )
 }
