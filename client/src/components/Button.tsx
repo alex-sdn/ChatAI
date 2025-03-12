89d5a0966765208cf13419769200c84c62d684ac
Alexandre S <alexsdnwcz@gmail.com>
2025-03-12 15:44:31 +0100

    move assets for diff paths

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -3,7 +3,7 @@ export default function Button({ children }) {
   return (
     <button className="w-9 h-9 flex items-center justify-center rounded-[6px] mx-[5px] hover:cursor-pointer hover:bg-[#3a3a3a]">
       <img
-        src={`src/assets/${children}.svg`}
+        src={`/${children}.svg`}
         alt={`{children} icon`}
         className="filter invert opacity-50 w-7 h-7"
       />
