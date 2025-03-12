d3b026ad4c272110960f16daadf1c13c25444f71
Alexandre S <alexsdnwcz@gmail.com>
2025-03-12 17:35:57 +0100

    Closeable sidebar

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,112 +1,36 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
+import Cookies from "js-cookie"
 import Button from "../components/Button"
-
-const chats = [
-  {
-    id: 1,
-    title: "Getting Started",
-    createdAt: new Date().toISOString()
-  },
-  {
-    id: 2,
-    title: "Project Discussion",
-    createdAt: new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString() // yesterday
-  },
-  {
-    id: 3,
-    title: "Code Review",
-    createdAt: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000).toISOString() // 2 days ago
-  },
-  {
-    id: 4,
-    title: "Debugging Session For Long Title",
-    createdAt: new Date(Date.now() - 3 * 24 * 60 * 60 * 1000).toISOString() // 3 days ago
-  },
-  {
-    id: 5,
-    title: "General Inquiry",
-    createdAt: new Date(Date.now() - 4 * 24 * 60 * 60 * 1000).toISOString() // 4 days ago
-  },
-  {
-    id: 6,
-    title: "Feature Request",
-    createdAt: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000).toISOString() // 5 days ago
-  },
-  {
-    id: 7,
-    title: "Bug Report",
-    createdAt: new Date(Date.now() - 6 * 24 * 60 * 60 * 1000).toISOString() // 6 days ago
-  },
-  {
-    id: 8,
-    title: "Team Meeting",
-    createdAt: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString() // 7 days ago
-  },
-  {
-    id: 9,
-    title: "Client Feedback",
-    createdAt: new Date(Date.now() - 8 * 24 * 60 * 60 * 1000).toISOString() // 8 days ago
-  },
-  {
-    id: 10,
-    title: "Deployment Issues",
-    createdAt: new Date(Date.now() - 9 * 24 * 60 * 60 * 1000).toISOString() // 9 days ago
-  }
-]
+import { useState } from "react"
+import Sidebar from "../components/Sidebar"
 
 const RootLayout = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove("access_token");
+    navigate("/sign-in", { replace: true });
+  }
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <div className="flex">
-      <aside className="flex flex-col bg-[#171717] min-h-screen max-h-screen w-3xs text-[15px] px-[15px]">
-        <header className="my-[10px]">
-          <div className="flex justify-between text-2xl">
-            <div>
-              <Button>sidebar</Button>
-            </div>
-            <div className="flex">
-              <Button>search</Button>
-              <Button>newChat</Button>
-            </div>
-          </div>
-        </header>
-
-        <h1 className="py-[10px] text-center font-semibold">
-          My Chats
-        </h1>
-        <hr className="border-[#3a3a3a]"/>
-
-        <div className="mt-[14px] flex-grow overflow-y-scroll overflow-x-hidden">
-          <ul>
-            {chats.map(chat => (
-              <li 
-                key={chat.id}
-                className="px-[8px] py-[8px] overflow-hidden whitespace-nowrap text-ellipsis
-                  hover:cursor-pointer hover:bg-[#2f2f2f] rounded-[8px]"
-              >
-                {chat.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <footer className="mt-auto py-[10px]">
-          <p className="text-[14px]">Upgrade plan</p>
-          <p className="text-[13px] opacity-60">More access to the best models</p>
-        </footer>
-      </aside>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
 
       <div className="flex flex-col w-full">
         <header className="flex justify-between my-[12px] mx-[22px]">
-          <p className="font-semibold text-[19px] opacity-60">
-            ChatAI
-          </p>
-          <button className="w-9 h-9 flex items-center justify-center rounded-[20px] mx-[5px] hover:cursor-pointer hover:bg-[#3a3a3a] border-gray-600/[0.6] border-[1px] bg-[#2a2a2a]">
-            <img
-              src={`src/assets/user.svg`}
-              alt="search icon"
-              className="filter invert opacity-50 w-5"
-            />
-          </button>
+          <div className="flex">
+            {!isOpen && <Button onClick={toggleSidebar}>sidebar</Button>}
+            <p className="font-semibold text-[19px] opacity-60 mt-[3px] ml-1">
+              ChatAI
+            </p>
+          </div>
+          <Button onClick={handleLogout}>user</Button>
         </header>
 
         <div className="flex items-center justify-center flex-grow">
