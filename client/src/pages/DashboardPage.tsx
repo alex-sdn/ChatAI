ef022fb90ab42cb44fb640aa8f8bb54869ca4dda
Alexandre S <alexsdnwcz@gmail.com>
2025-03-13 16:39:17 +0100

    center dashboard

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -2,8 +2,8 @@ import NewPrompt from "../components/NewPrompt"
 
 const DashboardPage = () => {
   return (
-    <div className="max-w-full">
-      <h1 className="font-semibold text-3xl text-center">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] max-w-full">
+      <h1 className="font-semibold text-3xl text-white text-center mb-5">
         What can I help with?
       </h1>
       <NewPrompt />
