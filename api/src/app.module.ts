471526bec259b2fd4d2ed2c7159bc996ad6cc83c
Alexandre S <alexsdnwcz@gmail.com>
2025-03-22 22:49:38 +0100

    add rate limiting

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,13 +5,27 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
+import { RateLimiterGuard, RateLimiterModule } from 'nestjs-rate-limiter';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule, 
-    PrismaModule, ChatModule],
+    RateLimiterModule.register({
+      points: 60,
+      duration: 60
+    }),
+    AuthModule,
+    ChatModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RateLimiterGuard
+    }
+  ],
 })
 export class AppModule {}
--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Get, Post, UseGuards, ValidationPipe } from '@nestjs/
 import { SignUpDto } from './dto/signup.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
+import { RateLimit } from 'nestjs-rate-limiter';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @Post('signup')
+    @RateLimit({ points: 25, duration: 360, keyPrefix: 'signup-hour' })
     signup(@Body(new ValidationPipe()) dto: SignUpDto) {
         return this.authService.signup(dto.username, dto.password, dto.confirmPassword);
     }
 
     @Post('signin')
+    @RateLimit({ points: 10, duration: 60, keyPrefix: 'signin-minute' })
     signin(@Body(new ValidationPipe()) dto: SignInDto) {
         return this.authService.signin(dto.username, dto.password);
     }
