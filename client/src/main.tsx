55fb4f5ebfadb5971983aaef49d6c121e1963df4
Alexandre S <alexsdnwcz@gmail.com>
2025-03-27 22:15:59 +0100

    StrictMode on

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
     <RouterProvider router={router} />
-  // </StrictMode>,
+  </StrictMode>,
 )
